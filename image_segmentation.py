445d74ee8bea672fe8dbd5bc72d5d16258f597d7
Guilherme Gobbo <136920721+guilhermegobbo@users.noreply.github.com>
2023-10-27 10:43:25 -0300

    Update image_segmentation.py

--- image_segmentation.py
+++ image_segmentation.py
@@ -10,7 +10,7 @@ image_reshaped = image.reshape(-1, 3)
 
 # n_clusters represents the total colors we want to identify
 # n_init we can say that it's for improving our cluster_centers
-model = KMeans(n_clusters=7, n_init=5)
+model = KMeans(n_clusters=5, n_init=5)
 model.fit(image_reshaped)
 
 new_img = model.cluster_centers_[model.labels_]
